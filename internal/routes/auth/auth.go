a7b4b904233b2df8499a213ca12c1cd40af41ea0
percoguru <gaurav.mehra1@outlook.com>
2021-09-10 22:35:22 +0530

    feat: first cut into auth

---
+++ internal/routes/auth/auth.go
@@ -0,0 +1,10 @@
+package authRoutes
+
+import "github.com/gofiber/fiber/v2"
+
+func SetUpAuthRoutes(router fiber.Router) {
+	// auth := router.Group("/auth")
+
+	// auth.Post("/login", authHandler.get)
+
+}
