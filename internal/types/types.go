68d8de3d4f38a7ad2a95b07a4ea99f9c1ca3a0fc
percoguru <gaurav.mehra1@outlook.com>
2021-10-04 22:51:06 +0530

    feat: add formbody type

--- internal/types/types.go
+++ internal/types/types.go
@@ -1,6 +1,13 @@
 package types
 
+import "github.com/percoguru/form-it-backend/internal/model"
+
 type FormEntity struct {
 	Title       string
 	PlaceHolder string
+}
+
+type FormBody struct {
+	Form      model.Form       `json:"form"`
+	FormPages []model.FormPage `json:"form-pages"`
 }
